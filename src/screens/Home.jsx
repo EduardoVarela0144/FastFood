46d7dd5a55c499b131b639a09f30620eeac53be1
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-09-09 12:50:42 -0600

    "Animations"

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,15 @@
 import {Text, SafeAreaView} from 'react-native'
+import Lottie from "lottie-react-native"
 
 export default function Home(){
     return(
-        <SafeAreaView>
-            <Text>Home</Text>
+        <SafeAreaView style={{backgroundColor:"white"}}>
+            <Lottie
+                autoPlay
+                lopp
+                source={require("../animations/animation.json")}
+                style={{width:"100%", height:"100%"}}
+            />
         </SafeAreaView>
     )
 }
