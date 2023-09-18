d9b2a322d612d559365a543f06453efc56fae022
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-09-17 18:01:39 -0600

    ScreenOption

--- src/navigation/NavigationTap.jsx
+++ src/navigation/NavigationTap.jsx
@@ -1,33 +1,50 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import Home from "../screens/Home"
-import Cart from "../screens/Cart"
-import Notifications from "../screens/Notifications"
-import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Cart from "../screens/Cart";
+import Notifications from "../screens/Notifications";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
-export default function NavigationTap(){
-    return(
+export default function NavigationTap() {
+  return (
     <Tab.Navigator
-        screenOptions={({route}) => ({
-            tabBarIcon:({focused,color,size}) => {
-                let iconName;
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-                if(route.name === "Home"){
-                    iconName="food"
-                }else if(route.name === "Cart"){
-                    iconName="cart"
-                }else if(route.name === "Notifications"){
-                    iconName = "message"
-                }
-                return <MaterialCommunityIcons name={iconName} size={size} color={color} />
-
-            }
-        })}
+          if (route.name === "Home") {
+            iconName = "food";
+          } else if (route.name === "Cart") {
+            iconName = "cart";
+          } else if (route.name === "Notifications") {
+            iconName = "message";
+          }
+          return (
+            <MaterialCommunityIcons name={iconName} size={size} color={color} />
+          );
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#FBBF24",
+      }}
     >
-        <Tab.Screen name="Cart" component={Cart}/>
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Notifications" component={Notifications}/>
-    </Tab.Navigator>)
-
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,7 +2,7 @@ import { Text, View, SafeAreaView } from "react-native";
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ backgroundColor: "white" }}>
+    <SafeAreaView className="mt-12">
       <View className="bg-red-500">
         <Text className="text-white">Tailwind</Text>
       </View>
