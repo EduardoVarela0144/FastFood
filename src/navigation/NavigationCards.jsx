e8d2288267522fdee3d88a77b5f522dcb4831e48
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-01 17:21:25 -0600

    Stack cards

---
+++ src/navigation/NavigationCards.jsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import ProductInfo from "../screens/ProductInfo";
+const Stack = createNativeStackNavigator();
+
+export default function NavigationCards() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="Product Info"
+        component={ProductInfo}
+        options={{ title: "", headerTransparent: true }}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/ProductInfo.jsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+export default function ProductInfo(){
+    return(
+        <SafeAreaView className="flex-1 justify-center items-center">
+            <View>
+                <Text>Hola mundo!</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
