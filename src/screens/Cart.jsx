700356f551a54fa3a29c935f46d5a05c89223d02
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-14 11:55:16 -0600

    Contador

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,9 +1,20 @@
-import {Text, SafeAreaView} from 'react-native'
+import {Text, SafeAreaView, Button} from 'react-native'
+import { useEffect, useState } from 'react'
 
 export default function Cart(){
+    const [contador, setContador ] = useState(0)
+
+    useEffect(()=>{
+        if(contador > 0){
+            alert('Cambie el estado del contador')
+        }
+    },[contador])
+
     return(
         <SafeAreaView>
             <Text>Cart</Text>
+            <Text>Contador: {contador}</Text>
+            <Button onPress={() => setContador(contador + 1) }title="Aumenta" />
         </SafeAreaView>
     )
 }
