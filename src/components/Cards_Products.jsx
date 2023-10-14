010a36e6dff5adc3d78d14c0ddf8fbaacba6d2af
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-14 12:43:35 -0600

    Lottie

---
+++ src/components/Cards_Products.jsx
@@ -0,0 +1,11 @@
+
+import { View, Text } from "react-native"
+export default function Cards_Products(){
+
+    return (
+    <View>
+        <Text>hola mundo</Text>
+    </View>
+    )
+
+}
--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,20 +1,26 @@
-import {Text, SafeAreaView, Button} from 'react-native'
-import { useEffect, useState } from 'react'
+import { Text, SafeAreaView, Button, View, FlatList } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import Cards_Products from "../components/Cards_Products";
+import { CartContext } from "../context/CartContext";
 
-export default function Cart(){
-    const [contador, setContador ] = useState(0)
-
-    useEffect(()=>{
-        if(contador > 0){
-            alert('Cambie el estado del contador')
-        }
-    },[contador])
-
-    return(
-        <SafeAreaView>
-            <Text>Cart</Text>
-            <Text>Contador: {contador}</Text>
-            <Button onPress={() => setContador(contador + 1) }title="Aumenta" />
-        </SafeAreaView>
-    )
+export default function Cart() {
+  const { Cart, setCart } = useContext(CartContext);
+  return (
+    <SafeAreaView className="flex-1 flex">
+      <View className="px-4 flex-1">
+        <Text className="font-bold text-2xl">Carrito de compras</Text>
+        <FlatList
+          data={Cart}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Cards_Products />}
+          ListEmptyComponent={
+            <View className="bg-red-500  justify-center items-center">
+              <Text>Aun no tienes productos en el carrito</Text>
+            </View>
+          }
+        />
+        <Cards_Products />
+      </View>
+    </SafeAreaView>
+  );
 }
