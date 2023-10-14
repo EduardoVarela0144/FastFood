4ccf8d178aed84bf23382c770d7e7bc6e8524f1d
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-14 13:26:28 -0600

    "Cart Logic"

--- src/components/Cards_Products.jsx
+++ src/components/Cards_Products.jsx
@@ -1,12 +1,40 @@
+import { View, Text, TouchableOpacity } from "react-native";
+export default function Cards_Products({ item, setCart }) {
 
-import { View, Text } from "react-native"
-export default function Cards_Products({item}){
+  const changueQuantity = (item, amount) => {
+    setCart((prevCart) =>
+      prevCart.map((cartItem) =>
+        cartItem.id === item.id
+          ? { ...cartItem, quantity: cartItem.quantity + amount }
+          : cartItem
+      )
+    );
+  };
 
-    return (
-    <View className="bg-green-200">
-        <Text>{item.name}</Text>
-        <Text>{item.quantity}</Text>
+
+  return (
+    <View className="bg-amber-400 h-28 justify-center rounded-2xl px-4 shadow-sm">
+      <View className=" w-full flex flex-row justify-between">
+        <View className="flex-1 ">
+          <Text className="font-bold text-white text-xl">{item.name}</Text>
+          <Text className="font-bold text-2xl">$ {item.price} MXN</Text>
+        </View>
+        <View className="flex-1 flex-row items-center justify-cente">
+          <View className=" flex-1 items-center">
+            <TouchableOpacity onPress={() => changueQuantity(item, -1)} className="bg-red-500 w-8 h-8 items-center justify-center rounded-full">
+              <Text className="font-bold text-xl text-white">-</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="bg-slate-100 flex-1 items-center rounded-lg py-2">
+            <Text className="font-bold">{item.quantity}</Text>
+          </View>
+          <View className=" flex-1 items-center">
+            <TouchableOpacity onPress={() => changueQuantity(item, 1)} className="bg-green-500 w-8 h-8 items-center justify-center rounded-full">
+              <Text className="font-bold text-xl text-white">+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-    )
-
+  );
 }
--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -5,14 +5,29 @@ import { CartContext } from "../context/CartContext";
 import Lottie from "lottie-react-native";
 export default function Cart() {
   const { Cart, setCart } = useContext(CartContext);
+
+  let total = 0;
+
+  Cart.forEach((cartItem) => {
+    const itemTotal = cartItem.quantity * cartItem.price;
+    total += itemTotal;
+  });
+
+
   return (
     <SafeAreaView className="flex-1 flex">
       <View className="px-4 flex-1">
-        <Text className="font-bold text-2xl">Carrito de compras</Text>
+        <Text className="font-bold text-2xl mb-6 text-center">
+          Carrito de compras
+        </Text>
+        <Text className="font-bold text-xl px-4 my-4">Total : $ {total} MXN</Text>
         <FlatList
           data={Cart}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (<Cards_Products item={item} />) }
+          renderItem={({ item }) => (
+            <Cards_Products item={item} setCart={setCart} />
+          )}
+          ItemSeparatorComponent={<View className="h-6" />}
           ListEmptyComponent={
             <View className="items-center">
               <Lottie
@@ -21,7 +36,9 @@ export default function Cart() {
                 style={{ width: 400, height: 400 }}
                 source={require("../animations/Cart.json")}
               />
-              <Text className="font-semibold text-xl text-center absolute mt-20">Aun no tienes productos en el carrito 😖</Text>
+              <Text className="font-semibold text-xl text-center absolute mt-20">
+                Aun no tienes productos en el carrito 😖
+              </Text>
             </View>
           }
         />
