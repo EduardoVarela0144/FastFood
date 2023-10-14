f43cbdae894596f5b424af654ae0235a5ffe6c72
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-14 11:50:18 -0600

    Create Context

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import {NavigationContainer} from "@react-navigation/native"
+import { NavigationContainer } from "@react-navigation/native";
 import NavigationTap from "./src/navigation/NavigationTap";
+import { CartContext } from "./src/context/CartContext";
+import { useState } from "react";
 
 export default function App() {
-
+  const [Cart, setCart] = useState([]);
   return (
-    <NavigationContainer>
-      <StatusBar style="auto"/>
-      <NavigationTap/>
-    </NavigationContainer>
-   
+    <CartContext.Provider value={{ Cart, setCart }}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <NavigationTap />
+      </NavigationContainer>
+    </CartContext.Provider>
   );
 }
-
