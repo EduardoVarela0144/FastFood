5cd13e626d1b1b9a021922b0ebb969978bfbc313
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-25 14:46:51 -0600

    "Add payment icon changue"

--- src/screens/Home/Cart.jsx
+++ src/screens/Home/Cart.jsx
@@ -1,9 +1,10 @@
-import { Text, SafeAreaView, View, FlatList } from "react-native";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
 import { useContext } from "react";
 import CardsCart from "../../components/Home/CardsCart";
 import { CartContext } from "../../context/CartContext";
 import Lottie from "lottie-react-native";
 import PageLayout from "../../components/General/PageLayout";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Cart() {
   const { Cart, setCart } = useContext(CartContext);
@@ -36,7 +37,7 @@ export default function Cart() {
                   source={require("../../animations/Cart.json")}
                 />
               </View>
-              
+
               <Text className=" text-md text-center">
                 Aun no tienes productos en el carrito 😖
               </Text>
@@ -48,6 +49,11 @@ export default function Cart() {
             Total : $ {total} MXN
           </Text>
         </View>
+        <TouchableOpacity className="bg-black rounded-xl w-full py-4 px-4 mt-4 flex-row space-x-2 justify-center">
+          <MaterialCommunityIcons name="credit-card-outline" size={20} color={"white"} />
+
+          <Text className="text-white">Pagar ahora</Text>
+        </TouchableOpacity>
       </View>
     </PageLayout>
   );
--- src/screens/Home/ProductInfo.jsx
+++ src/screens/Home/ProductInfo.jsx
@@ -1,5 +1,4 @@
 import {
-  SafeAreaView,
   Text,
   View,
   Image,
