9325d8c334f8f609f69e497ef73238519953bbb7
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-24 19:49:46 -0600

    Login design

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { SafeAreaView, Text, View, Dimensions } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import Lottie from "lottie-react-native";
 
 export default function Login() {
   return (
-    <View className="items-center">
-      <Lottie
-        autoPlay
-        loop
-        style={{ width: 400, height: 400 }}
-        source={require("../animations/Cart.json")}
-      />
-      <Text className="font-semibold text-xl text-center absolute mt-20">
-        Aun no tienes productos en el carrito 😖
-      </Text>
+    <View className="items-center flex flex-1">
+      <View className="w-full flex-1 bg-amber-400">
+        <Lottie
+          autoPlay
+          loop
+          resizeMode="cover"
+          style={{ flex: 1 }}
+          source={require("../animations/LoginAnimation.json")}
+        />
+      </View>
+      <View className="w-full flex-1 items-center justify-center">
+        <View className="w-full px-10">
+          <TouchableOpacity className="bg-orange-500 rounded-full w-full items-center py-2">
+            <Text className="text-white font-bold text-xl">Iniciar sesión</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
