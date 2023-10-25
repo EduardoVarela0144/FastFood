b9cbc4cbbfe26e94cb62df00e4399dae4f42ea7e
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-25 02:42:45 -0600

    Stepper Fix

--- src/screens/Register/Stepper.jsx
+++ src/screens/Register/Stepper.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, Text, View, TouchableOpacity } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { ROLES_TRANSLATE } from "../../config";
@@ -7,9 +7,23 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function Stepper() {
   const route = useRoute();
   const { rol } = route.params;
+
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = [
+    { title: "Paso 1", completed: currentStep >= 1 },
+    { title: "Paso 2", completed: currentStep >= 2 },
+    { title: "Paso 3", completed: currentStep >= 3 },
+    { title: "Paso 4", completed: currentStep >= 4 },
+  ];
+
+  const changeStep = (step) => {
+    setCurrentStep(step);
+  };
+
   return (
-    <SafeAreaView className="flex flex-1  px-4">
-      <View className="px-4 justify-center flex-1 ">
+    <SafeAreaView className="flex flex-1 px-4">
+      <View className=" justify-center flex-1 p-2">
         <Text className="font-semibold text-4xl">
           Completa tu registro como
           <Text className="font-bold text-orange-500">
@@ -17,12 +31,35 @@ export default function Stepper() {
             {ROLES_TRANSLATE[rol]}
           </Text>
         </Text>
-        <View className=" flex-1">
-          <Text>h</Text>
+        <View className="flex-1  py-4 space-y-4">
+          <View className="flex flex-row justify-between ">
+            {steps.map((step, index) => (
+              <View key={index} className="flex items-center justify-center flex-row">
+                <View
+                  className={`w-10 h-10 rounded-full border-2 ${
+                    step.completed ? "border-orange-500" : "border-gray-300"
+                  } items-center justify-center`}
+                >
+                  <Text>{index + 1}</Text>
+                </View>
+                {index < steps.length - 1 && (
+                  <View
+                    className={`h-2 w-16 ${
+                      step.completed ? "bg-orange-500" : "bg-gray-300"
+                    }`}
+                  ></View>
+                )}
+              </View>
+            ))}
+          </View>
+          <View className=" flex-1">
+            <Text>Renderiza componente</Text>
+          </View>
         </View>
-        <View className=" items-center">
+        <View className="items-end">
           <TouchableOpacity
             className="bg-white w-20 h-20 rounded-full items-center justify-center"
+            onPress={() => changeStep(currentStep < steps.length ? currentStep + 1 : currentStep)}
           >
             <MaterialCommunityIcons
               name="arrow-right"
