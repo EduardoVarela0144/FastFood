96458b1b1b201fc67e1775dc97236a0f8ff103e7
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-25 02:29:02 -0600

    Stepper logic

--- src/screens/Register/Stepper.jsx
+++ src/screens/Register/Stepper.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, TouchableOpacity } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { ROLES_TRANSLATE } from "../../config";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Stepper() {
   const route = useRoute();
   const { rol } = route.params;
   return (
-    <SafeAreaView>
-      <Text>Hola mundo {rol}</Text>
+    <SafeAreaView className="flex flex-1  px-4">
+      <View className="px-4 justify-center flex-1 ">
+        <Text className="font-semibold text-4xl">
+          Completa tu registro como
+          <Text className="font-bold text-orange-500">
+            {" "}
+            {ROLES_TRANSLATE[rol]}
+          </Text>
+        </Text>
+        <View className=" flex-1">
+          <Text>h</Text>
+        </View>
+        <View className=" items-center">
+          <TouchableOpacity
+            className="bg-white w-20 h-20 rounded-full items-center justify-center"
+          >
+            <MaterialCommunityIcons
+              name="arrow-right"
+              size={36}
+              color={"black"}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
