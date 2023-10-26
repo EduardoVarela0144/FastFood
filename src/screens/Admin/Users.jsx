b36d5518de1ed1b31efd68ab625eafe740a7eaf0
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-10-25 19:36:10 -0600

    Users edit and delete

--- src/screens/Admin/Users.jsx
+++ src/screens/Admin/Users.jsx
@@ -1,112 +1,52 @@
-// import React from 'react'
-// import { Text, View, FlatList } from 'react-native'
-// import PageLayout from '../../components/General/PageLayout'
-// import { useGetAllUsers } from '../../hooks/Users/useGetAllUsers'
-// import UserCard from './components/UserCard'
-// import { SwipeListView } from 'react-native-swipe-list-view';
-
-// export default  function Users() {
-//   const { data } = useGetAllUsers();
-
-//     return (
-//       <PageLayout>
-//         <View className="px-4 flex-1">
-//         <FlatList
-//           showsVerticalScrollIndicator={false}
-//           contentContainerStyle={{
-//             paddingBottom: 80,
-//           }}
-//           data={data}
-//           keyExtractor={(item) => item._id}
-//           className="py-4"
-//           renderItem={({ item }) => (
-//             <UserCard item={item}/>
-//           )}
-//           ItemSeparatorComponent={<View className="h-3" />}
-//         />
-       
-//         </View>
-//       </PageLayout>
-//     )
-// }
-
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import PageLayout from "../../components/General/PageLayout";
+import { useGetAllUsers } from "../../hooks/Users/useGetAllUsers";
+import UserCard from "./components/UserCard";
 import { SwipeListView } from "react-native-swipe-list-view";
 
-const data = [
-  { id: "1", text: "Item 1" },
-  { id: "2", text: "Item 2" },
-  { id: "3", text: "Item 3" },
-  { id: "4", text: "Item 4" },
-];
+export default function Users() {
+  const { data } = useGetAllUsers();
 
-const SwipeToDeleteEdit = () => {
-  const [items, setItems] = useState(data);
-
-  const onEdit = (item) => {
-    // Implement edit logic here
-    alert("Edit: " + item.text);
-  };
-
-  const onDelete = (item) => {
-    const updatedItems = items.filter((i) => i.id !== item.id);
-    setItems(updatedItems);
-  };
-
-  const renderItem = (data, rowMap) => {
+  const renderHiddenItem = (data) => {
     const { item } = data;
     return (
-      <View style={styles.rowFront}>
-        <Text>{item.text}</Text>
-      </View>
-    );
-  };
-
-  const renderHiddenItem = (data, rowMap) => {
-    const { item } = data;
-    return (
-      <View style={styles.rowBack}>
+      <View className="bg-white items-center flex-1 justify-between">
         <TouchableOpacity
+        className="rounded-r-xl"
           style={[styles.backRightBtn, styles.backRightBtnRight]}
           onPress={() => onDelete(item)}
         >
-          <Text style={styles.backTextWhite}>Delete</Text>
+          <Text style={styles.backTextWhite}>Eliminar</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={[styles.backRightBtn, styles.backRightBtnLeft]}
           onPress={() => onEdit(item)}
         >
-          <Text style={styles.backTextWhite}>Edit</Text>
+          <Text className="text-white" >Editar</Text>
         </TouchableOpacity>
       </View>
     );
   };
 
   return (
-    <View style={styles.container}>
-      <SwipeListView
-        data={items}
-        renderItem={renderItem}
-        renderHiddenItem={renderHiddenItem}
-        leftOpenValue={10}
-        rightOpenValue={-150}
-      />
-    </View>
+    <PageLayout>
+      <View className="px-4 flex-1">
+        <SwipeListView
+          data={data}
+          renderItem={UserCard}
+          renderHiddenItem={renderHiddenItem}
+          leftOpenValue={0}
+          rightOpenValue={-150}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={<View className="h-6"/>}
+        />
+      </View>
+    </PageLayout>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f4f4f4",
-  },
-  rowFront: {
-    backgroundColor: "white",
-    padding: 15,
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-  },
   rowBack: {
     alignItems: "center",
     backgroundColor: "white",
@@ -126,7 +66,7 @@ const styles = StyleSheet.create({
     width: 75,
   },
   backRightBtnLeft: {
-    backgroundColor: "blue",
+    backgroundColor: "#3a6ea6",
     right: 75,
   },
   backRightBtnRight: {
@@ -137,5 +77,3 @@ const styles = StyleSheet.create({
     color: "#FFF",
   },
 });
-
-export default SwipeToDeleteEdit;
